d96c574570b54012068eab9e322ec32ccbed30f8
creasedigital <ojorechris@gmail.com>
2022-03-08 11:31:14 +0100

    basic types

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,24 @@
 "use strict";
+// console.log('ID:', id)
+// Basic Types
 let id = 5;
-console.log('ID:', id);
+let company = 'Ojore';
+let isPublished = true;
+let x = 'Hello';
+//setting the value to any will make it reassignable
+x = true;
+// variables can be initialized with TS
+let age;
+age = 305;
+// Arrays can have types value declared 
+let ids = [1, 2, 3, 4];
+let arr = [1, true, 'hello'];
+// Tuple
+let person = [1, 'James', false];
+// Tuple Array
+let employee;
+employee = [
+    [19, 'Jones'],
+    [25, 'Jon'],
+    [38, 'Simeone'],
+];
--- index.ts
+++ index.ts
@@ -1,3 +1,29 @@
-let id:number = 5
 
-console.log('ID:', id)
+// console.log('ID:', id)
+// Basic Types
+let id:number = 5
+let company:string = 'Ojore'
+let isPublished: boolean = true
+let x:any = 'Hello'
+//setting the value to any will make it reassignable
+x = true
+
+// variables can be initialized with TS
+let age: number
+
+age=305
+
+// Arrays can have types value declared 
+let ids: number[] = [1,2,3,4]
+let arr:any[] = [1, true, 'hello']
+
+// Tuple
+let person: [number, string, boolean] = [1, 'James', false]
+// Tuple Array
+let employee: [number, string][]
+
+employee = [
+  [19, 'Jones'],
+  [25, 'Jon'],
+  [38, 'Simeone'],
+]
